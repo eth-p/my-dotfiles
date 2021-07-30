a4b69c15e553e735c84a795ee1ff2c6292463488
Ethan P <eth-p+git@hidden.email>
2021-07-29 21:14:14 -0700

    fish: session_var now removes stale session vars

--- home/.config/fish/conf.d/10-ethp-session.fish
+++ home/.config/fish/conf.d/10-ethp-session.fish
@@ -5,13 +5,41 @@
 # shell in the pane that spawned this shell. This allows for certain
 # environment variables to be copied between shells.
 
-if functions -q session_var &>/dev/null
+if status --is-interactive && functions -q session_var &>/dev/null
+	set creator_session ""
+	set creator_tty (tty)
+
+	# Running with my tmux integrations?
 	if [ -n "$INIT_TMUX_PANE_CREATOR" ]
 		set -gx TMUX_PANE_CREATOR "$INIT_TMUX_PANE_CREATOR"
 		set -ge INIT_TMUX_PANE_CREATOR
-		session_var --import-from-tty=(
+		
+		set creator_tty (
 			tmux display-message -t "$TMUX_PANE_CREATOR" -p "#{pane_tty}"
 		)
 	end
+
+	# Get the session file associated with the pane creator tty.
+	set creator_session (session_var --file-from-tty="$creator_tty")
+		
+	# Get the creator pid from the session file.
+	if [ -f "$creator_session" ]
+		set creator_pid (
+			session_var --using-file="$creator_session" --meta _session_owner_pid
+		)
+
+		# Check that the owner's session advertised pid still exists.
+		if kill -0 "$creator_pid" &>/dev/null
+			session_var --import-from-file="$creator_session"
+		else
+			# If it's doesn't, we will assume that fish didn't exit cleanly
+			# last time and that the session variables are stale.
+			rm "$creator_session"
+		end
+	end
+
+	# Set the meta key to help future shell instnaces determine if the
+	# session they're inheriting from is stale (i.e. reused tty).
+	session_var --set --meta "_session_owner_pid" "$fish_pid"
 end
 
--- home/.config/fish/functions/session_var.fish
+++ home/.config/fish/functions/session_var.fish
@@ -1,17 +1,32 @@
+# my-dotfiles | Copyright (C) 2021 eth-p
+# Repository: https://github.com/eth-p/my-dotfiles
+
+# A function which saves variables to a session file.
+# By default, the session files are tied to the pty of the shell.
 
 function session_var
-	argparse -x 'set,erase,clear,file,list-with-values,list,import-from-file,import-from-tty' \
-		-x 'set,erase,clear,file,import-from-tty,without' \
-		's/set' 'x/export' 'e/erase' 'c/clear' \
-		'list' 'list-with-values' 'without=+' \
-		'file' 'using-file=' 'import-from-tty=' 'import-from-file=' \
+	argparse -x 'set,erase,clear,file,file-from-tty,list-with-values,list,import-from-file' \
+		-x 'set,erase,clear,file,without' \
+		-x 'file,file-from-tty' \
+		-x 'export,meta' \
+		's/set' 'x/export' 'e/erase' 'c/clear' 'meta' \
+		'list' 'list-with-values' 'with-meta' 'with-mode' 'without=+' \
+		'file-from-tty=' \
+		'file' 'using-file=' 'import-from-file=' \
 		-- $argv || return $status
+
+	# Flag: --file-from-tty
+	# Get the session file for a tty.
+	if [ -n "$_flag_file_from_tty" ]
+		set -l tty (string replace --regex '^/dev/([a-z0-9]+)' '$1' -- "$_flag_file_from_tty")
+		echo "$TMPDIR/fish_session_var/$tty"
+		return 0
+	end
 
 	# Get the session var file.
 	set -l file $_flag_using_file
 	if [ (count $file) -eq 0 ]
-		set -l tty (string replace --regex '^/dev/([a-z0-9]+)' '$1' -- (tty))
-		set file "$TMPDIR/fish_session_var/$tty"
+		set file (session_var --file-from-tty=(tty))
 
 		# Create the file if it doesn't exist.
 		if ! [ -f "$tty" ]
@@ -37,42 +52,51 @@ function session_var
 	# Lists all session variables with their values.
 	if [ -n "$_flag_list_with_values" ]
 		begin
-			if [ (count $_flag_without) -gt 0 ]
-				set -l without_key
-				set -l without_var
-				while read line
-					[ -n "$line" ] || continue
+			set -l without_key
+			set -l without_var
+			while read line
+				[ -n "$line" ] || continue
 
-					set -l split (string split --max=1 '=' "$line")
-					set -l var_key "$split[1]"
-					set -l var_val "$split[2]"
-					set -l var_mode ""
+				set -l split (string split --max=1 '=' "$line")
+				set -l var_key "$split[1]"
+				set -l var_val "$split[2]"
+				set -l var_mode ""
 
-					# Parse out the var mode (if one is set)
-					if string match --quiet "* *" -- "$var_key"
-						set -l split (string split --max=1 -- ' ' "$var_key")
-						set var_key "$split[2]"
+				# Parse out the var mode (if one is set)
+				if string match --quiet "* *" -- "$var_key"
+					set -l split (string split --max=1 -- ' ' "$var_key")
+					set var_mode "$split[1] "
+					set var_key "$split[2]"
+				end
+
+				# Don't print meta vars unless --meta is provided.
+				if [ -z "$_flag_meta" ] && [ "$var_mode" = "meta " ]
+					if [ -z "$_flag_with_meta" ]
+						continue
+					end
+				else if [ -n "$_flag_meta" ] && [ "$var_mode" != "meta " ]
+					continue
+				end
+
+				# If it's included in --without, skip it.
+				set without_var false
+				for without_key in $_flag_without
+					if [ "$var_key" = "$without_key" ]
+						set without_var true
+						break
+					end
+				end
+
+				# Print it.
+				if not $without_var
+					if [ -z "$_flag_with_mode" ]
+						set var_mode ""
 					end
 
-					# If it's included in --without, skip it.
-					set without_var false
-					for without_key in $_flag_without
-						if [ "$var_key" = "$without_key" ]
-							set without_var true
-							break
-						end
-					end
-
-					# Print it.
-					if not $without_var
-						printf "%s=%s\n" "$var_key" "$var_val"
-					end
-				end < "$file" || return 1
-				return 0
-			else
-				cat "$file" || return 1
-				return $status
-			end
+					printf "%s%s=%s\n" "$var_mode" "$var_key" "$var_val"
+				end
+			end < "$file" || return 1
+			return 0
 		end | sed '/^$/d; /^#/d'
 	end
 
@@ -81,7 +105,7 @@ function session_var
 	#
 	# Lists all session variables.
 	if [ -n "$_flag_list" ]
-		session_var --list-with-values --using-file="$file" \
+		session_var --list-with-values --using-file="$file" $_flag_with_meta $_flag_meta \
 			(string replace --regex -- '^' '--without=' $_flag_without) \
 			| sed 's/^[a-z]\{1,\} //' \
 			| cut -d'=' -f1
@@ -92,24 +116,20 @@ function session_var
 	# Usage: session_var --import-from-file=FILE
 	# 
 	# Imports all session variables associated with another tty.
-	if [ -n "$_flag_import_from_file" ] || [ -n "$_flag_import_from_tty" ]
+	if [ -n "$_flag_import_from_file" ]
 		set -l import_file "$_flag_import_from_file"
-		if [ -n "$_flag_import_from_tty" ]
-			set -l tty (string replace --regex '^/dev/([a-z0-9]+)' '$1' -- "$_flag_import_from_tty")
-			set import_file "$TMPDIR/fish_session_var/$tty"
-		end
 
 		set -l line
 		set -l current_var_key
 
 		# Cache the current session variables that won't be overwritten.
 		set -l vars_to_import (session_var --list --using-file="$import_file") || return $status
-		set -l vars_and_vals_to_keep (session_var --list-with-values --using-file="$file" \
+		set -l vars_and_vals_to_keep (session_var --list-with-values --with-meta --with-mode --using-file="$file" \
 			(string replace --regex -- '^' '--without=' $vars_to_import)
 		) || return $status
 
 		# Load the session variables to import.
-		set -l vars_and_vals_to_import (session_var --list-with-values --using-file="$import_file")
+		set -l vars_and_vals_to_import (session_var --list-with-values --with-mode --using-file="$import_file")
 
 		# "Import" the variables from both caches.
 		for line in $vars_and_vals_to_keep $vars_and_vals_to_import
@@ -129,6 +149,8 @@ function session_var
 				switch "$var_mode"
 					case "export "
 						set set_flags '-gx'
+					case "meta "
+						set set_flags ''
 					case "*"
 						echo "session_var: unknown variable mode '$var_mode'" 1>&2
 				end
@@ -140,7 +162,9 @@ function session_var
 			))
 
 			# Set the variable.
-			set "$set_flags" "$var_key" $var_val
+			if [ -n "$set_flags" ]
+				set "$set_flags" "$var_key" $var_val
+			end
 			printf "%s%s=%s\n" "$var_mode" "$var_key" "$var_val_encoded"
 		end > "$file"
 		
@@ -174,7 +198,7 @@ function session_var
 		end
 
 		# Update session file.
-		set -l vars (session_var --list-with-values --using-file="$file" --without="$var_key")
+		set -l vars (session_var --list-with-values --using-file="$file" --with-meta --with-mode --without="$argv[1]")
 		printf "%s\n" $vars > "$file"
 
 		# Update variable.
@@ -197,13 +221,19 @@ function session_var
 		set -l var_val $argv[2..-1]
 		set -l var_mode ''
 		set -l set_flags '-g'
+
+		# Option: --export
 		if [ -n "$_flag_export" ]
 			set var_mode 'export '
 			set set_flags '-gx'
 		end
 
+		if [ -n "$_flag_meta" ]
+			set var_mode 'meta '
+		end
+
 		# Update session file.
-		set -l vars (session_var --list-with-values --using-file="$file" --without="$var_key")
+		set -l vars (session_var --list-with-values --using-file="$file" --with-meta --with-mode --without="$var_key")
 		printf "%s\n" $vars > "$file"
 		printf "%s%s=%s\n" "$var_mode" "$var_key" (
 			string join "\n" (
@@ -212,9 +242,29 @@ function session_var
 		) >> "$file"
 		
 		# Update variable.
-		set "$set_flags" "$var_key" $var_val
+		if [ -z "$_flag_meta" ]
+			set "$set_flags" "$var_key" $var_val
+		end
 		return 0
 	end
+
+	# No flag (i.e. --get)
+	if [ (count $argv) -ne 1 ]
+		echo "session_var: get: variable name missing" 1>&2
+		return 1
+	end
+
+	set -l split (
+		string split --max=1 -- '=' \
+		(session_var --list-with-values --using-file="$file" $_flag_meta | grep "^$argv[1]=")
+	) || return 1
+
+	# Decode the variable and print.
+	string replace --regex --all '\\\\([\\\\ ])' '$1' -- (
+		string split "\n" -- "$split[2]"
+	) || true
+
+	return 0
 end
 
 function __session_var_destroy --on-event "fish_exit"
