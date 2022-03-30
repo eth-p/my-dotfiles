da3b6e5c23aba4831549016486e9ac9e32fabb0c
Ethan P <eth-p+git@hidden.email>
2022-03-30 03:32:24 -0700

    fish: Fix incorrect space/tab mix in fish_prompt

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,11 +1,11 @@
 function fish_prompt
 	promptfessional enable arrow
 
-    promptfessional section status --delimiter=' '
-        promptfessional show status
-        promptfessional show private
-        promptfessional show jobs
-        promptfessional show sudo
+	promptfessional section status --delimiter=' '
+		promptfessional show status
+		promptfessional show private
+		promptfessional show jobs
+		promptfessional show sudo
 	
 	promptfessional section cmdtime
 		promptfessional show cmdtime \
@@ -18,17 +18,17 @@ function fish_prompt
 				--pattern='{color} {label}{/context_if_multiple}{/bold_if_namespace}{namespace}³ '
 	end
 
-    promptfessional section path --pattern=' %s '
-        promptfessional show path \
-        	--collapse-home \
-        	--abbrev-parents \
-        	--decoration promptfessional_decoration_git \
-        	--git-hide-branch master \
+	promptfessional section path --pattern=' %s '
+		promptfessional show path \
+			--collapse-home \
+			--abbrev-parents \
+			--decoration promptfessional_decoration_git \
+			--git-hide-branch master \
 			--git-symbol-branch (printf "\uE0A0") \
 			--git-use-cache
 
-    promptfessional end
+	promptfessional end
 
 	set_color (string replace -- "--background=" "" (promptfessional color section.path --or=component.path --only-background --print))
-    promptfessional literal "{arrow}\x1B[0m "
+	promptfessional literal "{arrow}\x1B[0m "
 end
