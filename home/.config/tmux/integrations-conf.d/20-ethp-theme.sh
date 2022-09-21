a907ccfc1a42a60be12cdb2c68fe57160f746672
Ethan P <eth-p+git@hidden.email>
2022-09-21 11:53:40 -0700

    tmux: Add dark theme styles for integrations

---
+++ home/.config/tmux/integrations-conf.d/20-ethp-theme.sh
@@ -0,0 +1,28 @@
+# my-dotfiles | Copyright (C) 2022 eth-p
+# Repository: https://github.com/eth-p/my-dotfiles
+# =============================================================================
+#
+# Summary
+# -------
+#
+#   Customizes the theme of the integrations.
+#
+# How it's used in my-dotfiles
+# ----------------------------
+#
+#   Changes the theme to match my tmux config more closely.
+#
+# =============================================================================
+
+# shellcheck disable=SC2034
+case "${TERM_BG:-dark}" in
+dark) {
+	INTEGRATION_ERROR_STYLE='bg=colour52 fg=colour252'
+	ICFG_EXIT_PROMPT_STYLE='bg=colour94 fg=colour254'
+} ;;
+
+light) {
+	: # TODO
+} ;;
+esac
+
