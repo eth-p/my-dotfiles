a1b6426ebb9aab879f25582cb95e7a59c3a17ab0
Ethan P. <eth-p+git@hidden.email>
2024-06-10 17:06:06 -0700

    fish: Add more git aliases

--- home/.config/fish/conf.d/99-ethp-git-aliases.fish
+++ home/.config/fish/conf.d/99-ethp-git-aliases.fish
@@ -15,6 +15,8 @@
 #   git rc      -- Continue a rebase.
 #   git ss      -- Short status.
 #   git unstage -- Unstage files.
+#   git web     -- Open the repo in GitHub.
+#   git pr      -- Alias for `gh pr`.
 #
 # =============================================================================
 
@@ -23,7 +25,7 @@ if not status is-interactive
 end
 
 # Only update when necessary.
-set -l aliases_version 1
+set -l aliases_version 2
 if test "$__ethp_git_aliases_version" = "$aliases_version"
 	return
 end
@@ -36,3 +38,6 @@ git config --global alias.r 'rebase -i --autosquash'
 git config --global alias.rc 'rebase --continue'
 git config --global alias.ss 'status -s'
 git config --global alias.unstage 'reset HEAD --'
+
+git config --global alias.web '!gh browse'
+git config --global alias.pr '!gh pr'
