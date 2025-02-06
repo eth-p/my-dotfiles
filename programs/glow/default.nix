9e9011cb8e6aa3c6736d840a8c610e07042c2834
Ethan P. <eth-p+git@hidden.email>
2025-02-06 00:20:48 -0800

    feat(glow): Add base16 and bat-monokai styles

--- programs/glow/default.nix
+++ programs/glow/default.nix
@@ -3,15 +3,22 @@
 #
 # Program: https://github.com/charmbracelet/glow
 # ==============================================================================
-{ lib, pkgs, config, ctx, ... }:
+{ lib, pkgs, config, ctx, my-dotfiles, ... } @ inputs:
 let
-  inherit (lib) mkIf;
-  inherit (config.lib.file) mkOutOfStoreSymlink;
+  inherit (lib) mkIf strings attrsets;
   cfg = config.my-dotfiles.glow;
+  glowHome = "${config.xdg.configHome}/glow";
+  styles = (import ./styles.nix inputs);
 in
 {
   options.my-dotfiles.glow = with lib; {
     enable = mkEnableOption "install glow";
+
+    style = mkOption {
+      type = types.enum (styles.all);
+      description = "the style to use";
+      default = "base16";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -20,6 +27,30 @@ in
     home.packages = [
       pkgs.glow
     ];
+
+    # Configure glow.
+    home.file = {
+      "${glowHome}/glow.yml" =
+        {
+          text = ''
+            style: ${builtins.toJSON (styles.toFlag cfg.style)}
+            mouse: true
+            pager: true
+          '';
+        };
+    }
+
+    # Add the custom style files.
+    // (builtins.listToAttrs (
+      builtins.map
+        (style: {
+          name = "${glowHome}/styles/${style}.json";
+          value = {
+            source = ./styles + "/${style}.json";
+          };
+        })
+        styles.custom)
+    );
 
   };
 }
