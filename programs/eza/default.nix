645d99ba8691b09977baa80ef9f00015c3d7c142
Ethan P. <eth-p+git@hidden.email>
2025-02-07 00:40:40 -0800

    feat(eza): Add base16 theme

--- programs/eza/default.nix
+++ programs/eza/default.nix
@@ -5,13 +5,29 @@
 # ==============================================================================
 { lib, pkgs, config, ctx, ... }:
 let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf strings attrsets;
   inherit (config.lib.file) mkOutOfStoreSymlink;
+  ezaHome = "${config.xdg.configHome}/eza";
   cfg = config.my-dotfiles.eza;
+
+  # Read the available custom styles (themes).
+  # These are the YAML files under `./styles` without the file extension.
+  customStyles = builtins.map
+    (strings.removeSuffix ".yaml")
+    (builtins.attrNames (attrsets.filterAttrs
+      (name: kind: kind == "regular" && (strings.hasSuffix ".yaml" name))
+      (builtins.readDir ./styles)
+    ));
 in
 {
   options.my-dotfiles.eza = with lib; {
     enable = mkEnableOption "install and configure eza as a replacement for ls";
+
+    style = mkOption {
+      type = types.enum ([ "default" ] ++ customStyles);
+      description = "the style to use";
+      default = "base16";
+    };
 
     enableAliases = mkOption {
       type = types.bool;
@@ -20,20 +36,29 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf cfg.enable (lib.mkMerge [
 
     # Configure eza.
-    programs.eza = {
-      enable = true;
-      git = true; # uses libgit2
+    {
+      programs.eza = {
+        enable = true;
+        git = true; # uses libgit2
 
-      enableBashIntegration = cfg.enableAliases;
-      enableZshIntegration = cfg.enableAliases;
-      enableFishIntegration = cfg.enableAliases;
-      enableIonIntegration = cfg.enableAliases;
-      enableNushellIntegration = cfg.enableAliases;
-    };
+        enableBashIntegration = cfg.enableAliases;
+        enableZshIntegration = cfg.enableAliases;
+        enableFishIntegration = cfg.enableAliases;
+        enableIonIntegration = cfg.enableAliases;
+        enableNushellIntegration = cfg.enableAliases;
+      };
+    }
 
-  };
+    # Set style.
+    (mkIf (cfg.style != "default") {
+      home.file."${ezaHome}/theme.yml" = {
+        source = ./styles + "/${cfg.style}.yaml";
+      };
+    })
+
+  ]);
 
 }
