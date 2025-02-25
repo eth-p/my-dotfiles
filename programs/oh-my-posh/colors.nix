eb689989d05a17fc41bd999117162e3927b6ac81
Ethan P. <eth-p+git@hidden.email>
2025-02-24 16:06:56 -0800

    feat(oh-my-posh): Use conditional palettes

---
+++ programs/oh-my-posh/colors.nix
@@ -0,0 +1,14 @@
+{
+  dark = {
+    path_parent = "#99999";
+    path_curdir = "#ffffff";
+    path_bg = "#444444";
+    flags_bg = "#ffffff";
+    flags_exitcode_color = "#ff0000";
+    vcs_fg = "#ffffff";
+    vcs_modified = "#882211";
+    vcs_staged = "#995500";
+    vcs_synced = "#447711";
+    vcs_unknown = "#774444";
+  };
+}
--- programs/oh-my-posh/default.nix
+++ programs/oh-my-posh/default.nix
@@ -54,17 +54,9 @@ in
                 inherit generator;
               })
           );
-          palette = {
-            path_parent = "#99999";
-            path_curdir = "#ffffff";
-            path_bg = "#444444";
-            flags_bg = "#ffffff";
-            flags_exitcode_color = "#ff0000";
-            vcs_fg = "#ffffff";
-            vcs_modified = "#882211";
-            vcs_staged = "#995500";
-            vcs_synced = "#447711";
-            vcs_unknown = "#774444";
+          palettes = {
+            template = "dark";
+            list = (import ./colors.nix);
           };
         };
       };
