8854d1218120270f1483ad3f5ceaf7839d25d65f
Ethan P. <eth-p+git@hidden.email>
2025-03-08 18:37:23 -0800

    build: Use devenv for formatting instead of script

---
+++ devenv.nix
@@ -0,0 +1,68 @@
+# my-dotfiles | Copyright (C) 2025 eth-p
+# Repository: https://github.com/eth-p/my-dotfiles
+#
+# This provides a consistent environment for running development scripts.
+# ==============================================================================
+{ pkgs, lib, config, inputs, ... }:
+{
+  packages = [
+    pkgs.git
+  ];
+
+  # https://devenv.sh/tasks/
+  tasks = {
+    "my-dotfiles:format-nix" = {
+      exec = ''
+        '${pkgs.fd}/bin/fd' --glob '**/*.nix' \
+          --exec-batch '${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt' '{}'
+      '';
+    };
+
+    "my-dotfiles:format-sh" = {
+      exec = ''
+        declare -a scripts
+        readarray -t scripts < <({
+          '${pkgs.fd}/bin/fd' --glob '*' --type=executable scripts/
+          '${pkgs.fd}/bin/fd' --glob '*.sh' --hidden lib/bash/
+        })
+
+        '${pkgs.shfmt}/bin/shfmt' --write \
+          --language-dialect bash \
+          --indent 0 \
+          --space-redirects \
+          "''${scripts[@]}"
+      '';
+    };
+
+    "my-dotfiles:format-lua" = {
+      exec = ''
+        '${pkgs.stylua}/bin/stylua' programs/neovim
+      '';
+    };
+
+    "my-dotfiles:format-json" = {
+      exec = ''
+        '${pkgs.fd}/bin/fd' --glob '**/*.json' programs \
+          --exec-batch '${pkgs.nodePackages.prettier}/bin/prettier' '--write' '{}'
+      '';
+    };
+
+    "my-dotfiles:format-yaml" = {
+      exec = ''
+        '${pkgs.fd}/bin/fd'  --glob '**/*.{yml,yaml}' programs \
+          --exec-batch '${pkgs.nodePackages.prettier}/bin/prettier' '--write' '{}'
+      '';
+    };
+
+    "my-dotfiles:format" = {
+      exec = "true";
+      after = [
+        "my-dotfiles:format-json"
+        "my-dotfiles:format-yaml"
+        "my-dotfiles:format-lua"
+        "my-dotfiles:format-nix"
+        "my-dotfiles:format-sh"
+      ];
+    };
+  };
+}
