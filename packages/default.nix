d4ea9500944fc53aebfb40e5563375908553aca1
Ethan P. <eth-p+git@hidden.email>
2025-03-15 16:07:47 -0700

    feat: Add support for declaring packages in flake
    The packages will be used for utility scripts shared between
    different home-manager modules.

---
+++ packages/default.nix
@@ -0,0 +1,14 @@
+# my-dotfiles | Copyright (C) 2025 eth-p
+# Repository: https://github.com/eth-p/my-dotfiles
+#
+# This references all the packages under this directory,
+# calling a generator function for each target system.
+# ==============================================================================
+{ systems }: {
+  term-query-bg = let gen = import ./term-query-bg; in {
+    aarch64-darwin = gen systems.inputs.aarch64-darwin;
+    x86_64-darwin = gen systems.inputs.x86_64-darwin;
+    aarch64-linux = gen systems.inputs.aarch64-linux;
+    x86_64-linux = gen systems.inputs.x86_64-linux;
+  };
+}
---
+++ packages/term-query-bg/default.nix
@@ -0,0 +1,12 @@
+# my-dotfiles | Copyright (C) 2025 eth-p
+# Repository: https://github.com/eth-p/my-dotfiles
+#
+# Nix function to generate the package for my term-query-bg script.
+# ==============================================================================
+{ pkgs, system, ... }: pkgs.writeShellApplication {
+  name = "term-query-bg";
+
+  runtimeInputs = [ pkgs.bash ];
+
+  text = (builtins.readFile ./bin/term-query-bg.sh);
+}
