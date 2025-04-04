62ec9329fb3c95416c4d2cb1dc974c8ba29f82df
eth-p <me+git@eth-p.dev>
2025-04-03 22:40:12 -0700

    fix(git): Handle dyff errors gracefully

--- programs/git/diff_with_dyff.sh
+++ programs/git/diff_with_dyff.sh
@@ -6,5 +6,11 @@ printf "\033[34m"
 printf "%-$(tput cols)s" "" | sed 's/ /─/g'
 printf "\033[m\n"
 
+# Function to fallback to regular diff in the file cannot be parsed.
+fallback_diff() {
+    { diff -u "$2" "$5" || true; } | sed '/^\(+++\|---\)/d'
+}
+
 # Perform the diff.
-dyff --color on between --omit-header "$2" "$5";
+dyff --color on between --omit-header "$2" "$5" 2>/dev/null \
+    || fallback_diff "$@"
