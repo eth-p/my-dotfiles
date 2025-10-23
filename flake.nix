18f080bd38f9b72b6593e62694509fc4c6523d34
Ethan P. <eth-p+git@hidden.email>
2025-10-23 11:05:06 -0700

    chore: Update vicinae to v0.15.3

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
     # vicinae: https://github.com/vicinaehq/vicinae
-    vicinae.url = "github:vicinaehq/vicinae/v0.14.5";
+    vicinae.url = "github:vicinaehq/vicinae/v0.15.3";
 
     # My own packages:
     kubesel = {
