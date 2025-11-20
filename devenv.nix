1967c1c7b9499fdb55ae37f22c662fe72b1e8598
Ethan P. <eth-p+git@hidden.email>
2025-11-20 14:41:19 -0800

    build: Recreate devenv config

--- devenv.nix
+++ devenv.nix
@@ -11,34 +11,59 @@
   ...
 }:
 {
-  packages = [
-    pkgs.git
-
-    # Formatters
-    pkgs.treefmt
-    pkgs.nodePackages.prettier
-    pkgs.shfmt
-    pkgs.nixfmt-rfc-style
-    pkgs.stylua
-  ];
-
-  # https://devenv.sh/tasks/
-  tasks = {
-    "my-dotfiles:format" = {
-      exec = "treefmt";
-    };
-
-    "my-dotfiles:docs" = {
-      exec = ''
-        nix run github:Thunderbottom/nix-options-doc?rev=2caa4b5756a8666d65d70122f413e295f56886e7 -- \
-          --path "." \
-          --out OPTIONS.md \
-          --exclude-dir "lib" \
-          --exclude-dir "profiles" \
-          --filter-by-prefix "options.my-dotfiles" \
-          --strip-prefix "options." \
-          --sort
-      '';
-    };
+  options.fast = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
   };
+
+  config = lib.mkMerge [
+    {
+      packages = [
+        pkgs.git
+
+        # Formatters
+        pkgs.treefmt
+        pkgs.nodePackages.prettier
+        pkgs.shfmt
+        pkgs.nixfmt-rfc-style
+        pkgs.stylua
+      ];
+
+      # https://devenv.sh/tasks/
+      tasks = {
+        "my-dotfiles:format" = {
+          exec = "treefmt";
+        };
+      };
+    }
+
+    # Configuration applied when not using direnv.
+    (lib.mkIf (!config.fast) (
+      let
+        system = pkgs.stdenv.system;
+      in
+      {
+        packages = [
+          inputs.nix-options-doc.packages.${system}.default
+        ];
+
+        # https://devenv.sh/tasks/
+        tasks = {
+          "my-dotfiles:docs" = {
+            before = [ "my-dotfiles:format" ];
+            exec = ''
+              nix-options-doc \
+                --path "." \
+                --out OPTIONS.md \
+                --exclude-dir "lib" \
+                --exclude-dir "profiles" \
+                --filter-by-prefix "options.my-dotfiles" \
+                --strip-prefix "options." \
+                --sort
+            '';
+          };
+        };
+      }
+    ))
+  ];
 }
