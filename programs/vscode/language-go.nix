cde8241ddc56a4f80b72751567b855e1382f9f75
Ethan P. <eth-p+git@hidden.email>
2025-12-04 11:42:34 -0800

    feat(vscode/go): Add table test snippets

--- programs/vscode/language-go.nix
+++ programs/vscode/language-go.nix
@@ -127,45 +127,154 @@ in
 
     # Add snippets for general language features.
     (mkIf cfg.lsp.enable {
-      programs.vscode.profiles.default.languageSnippets.go = {
-        "New interface type" = {
-          "prefix" = "interface";
-          "description" = "Create an interface";
-          "body" = [
-            "type \${1:name} interface {"
-            "\t$0"
-            "}"
-          ];
-        };
+      programs.vscode.profiles.default.languageSnippets.go =
+        let
+          mkTableTestInputPlaceholderDeclarations =
+            placeholderNum: n:
+            if n == 0 then
+              [ ]
+            else
+              [ "\t\t\${${toString placeholderNum}:input${toString n}} \${${toString (placeholderNum + 1)}:ty}" ]
+              ++ (mkTableTestInputPlaceholderDeclarations (placeholderNum + 2) (n - 1));
 
-        "New struct type" = {
-          "prefix" = "struct";
-          "description" = "Create a struct";
-          "body" = [
-            "type \${1:name} struct {"
-            "\t$0"
-            "}"
-          ];
-        };
+          mkTableTestInputPlaceholderUsages =
+            placeholderNum: n:
+            if n == 1 then
+              "tc.\$${toString placeholderNum}"
+            else
+              "${mkTableTestInputPlaceholderUsages (placeholderNum + 2) (n - 1)}, \$${toString placeholderNum}";
 
-        "Receiver function" = {
-          "prefix" = "impl";
-          "description" = "Create a receiver function";
-          "body" = [
-            "func (\${1:s} *\${2:ty}) \${3:name}(\${4:params}) \${5:returns} {"
-            "\t$0"
-            "}"
-          ];
-        };
+          mkTableTestSnippet =
+            { params }:
+            let
+              paramsDesc = if params == 1 then "1 parameter" else "${toString params} parameters";
+              name = "Table test, ${paramsDesc}";
+            in
+            {
+              "${name}" = {
+                prefix = [
+                  "func Test${toString params}"
+                ];
+                description = "Creates a table test function with ${paramsDesc}";
+                body = [
+                  "func Test\${1:Name}(t *testing.T) {"
+                  "\ttestcases := map[string]struct {"
+                  "\t\texpected \${2:type}"
+                ]
+                ++ (mkTableTestInputPlaceholderDeclarations 3 params)
+                ++ [
+                  "\t}{"
+                  "\t\t\$0"
+                  "\t}"
+                  ""
+                  "\tt.Parallel()"
+                  "\tfor name, tc := range testcases {"
+                  "\t\tt.Run(name, func(t *testing.T) {"
+                  "\t\t\tt.Parallel()"
+                  "\t\t\tactual := \$1(${mkTableTestInputPlaceholderUsages 3 params})"
+                  "\t\t\tassert.Equal(t, tc.expected, actual)"
+                  "\t\t})"
+                  "\t}"
+                  "}"
+                ];
+              };
 
-        "Assert implements" = {
-          "prefix" = "assert interface";
-          "description" = "Asserts that a type implements the given interface";
-          "body" = [
-            "var _ \${1:interface} = (*\${2:ty})(nil) // Asserts \${2} implements \${1}"
-          ];
-        };
-      };
+              "${name} with error" = {
+                prefix = [
+                  "func Test${toString params}E"
+                ];
+                description = "Creates a table test function with ${paramsDesc}";
+                body = [
+                  "func Test\${1:Name}(t *testing.T) {"
+                  "\ttestcases := map[string]struct {"
+                  "\t\texpected \${2:type}"
+                  "\t\texpectedError error"
+                ]
+                ++ (mkTableTestInputPlaceholderDeclarations 3 params)
+                ++ [
+                  "\t}{"
+                  "\t\t\$0"
+                  "\t}"
+                  ""
+                  "\tt.Parallel()"
+                  "\tfor name, tc := range testcases {"
+                  "\t\tt.Run(name, func(t *testing.T) {"
+                  "\t\t\tt.Parallel()"
+                  "\t\t\tactual, err := \$1(${mkTableTestInputPlaceholderUsages 3 params})"
+                  "\t\t\trequire.ErrorIs(t, err, tc.expectedError)"
+                  "\t\t\tassert.Equal(t, tc.expected, actual)"
+                  "\t\t})"
+                  "\t}"
+                  "}"
+                ];
+              };
+            };
+
+        in
+        {
+          "New interface type" = {
+            "prefix" = "interface";
+            "description" = "Create an interface";
+            "body" = [
+              "type \${1:name} interface {"
+              "\t$0"
+              "}"
+            ];
+          };
+
+          "New struct type" = {
+            "prefix" = "struct";
+            "description" = "Create a struct";
+            "body" = [
+              "type \${1:name} struct {"
+              "\t$0"
+              "}"
+            ];
+          };
+
+          "Receiver function" = {
+            "prefix" = "impl";
+            "description" = "Create a receiver function";
+            "body" = [
+              "func (\${1:s} *\${2:ty}) \${3:name}(\${4:params}) \${5:returns} {"
+              "\t$0"
+              "}"
+            ];
+          };
+
+          "Assert implements" = {
+            "prefix" = "assert interface";
+            "description" = "Asserts that a type implements the given interface";
+            "body" = [
+              "var _ \${1:interface} = (*\${2:ty})(nil) // Asserts \${2} implements \${1}"
+            ];
+          };
+
+          "Table test" = {
+            prefix = "func Test";
+            description = "Test function with table tests";
+            body = [
+              "func Test\${1:Name}(t *testing.T) {"
+              "\ttestcases := map[string]struct {"
+              "\t\texpected \${2:type}"
+              "\t\t\$0"
+              "\t}{"
+              "\t\t"
+              "\t}"
+              ""
+              "\tt.Parallel()"
+              "\tfor name, tc := range testcases {"
+              "\t\tt.Run(name, func(t *testing.T) {"
+              "\t\t\tt.Parallel()"
+              "\t\t\t"
+              "\t\t})"
+              "\t}"
+              "}"
+            ];
+          };
+        }
+        // (mkTableTestSnippet { params = 1; })
+        // (mkTableTestSnippet { params = 2; });
     })
 
   ]);
