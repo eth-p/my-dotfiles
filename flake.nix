1e076d9d6f03cde6fa6c0bf2119d85fdeae37e25
Ethan P. <eth-p+git@hidden.email>
2026-08-24 21:40:03 +0000

    upgrade(vicinae): v0.23.2 -> v0.26.3

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
     # vicinae: https://github.com/vicinaehq/vicinae
-    vicinae.url = "github:vicinaehq/vicinae/v0.23.2";
+    vicinae.url = "github:vicinaehq/vicinae/v0.26.3";
 
     # My own packages:
     kubesel.url = "github:eth-p/kubesel";
